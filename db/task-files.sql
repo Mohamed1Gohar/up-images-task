9fa502090dade0527a237a9828ac600a713721b6
mohamed1gohar <mohamedgohar365@gmai.com>
2022-01-29 08:17:51 +0200

    update db

--- db/task-files.sql
+++ db/task-files.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 29, 2022 at 06:23 AM
+-- Generation Time: Jan 29, 2022 at 08:16 AM
 -- Server version: 8.0.27-0ubuntu0.20.04.1
 -- PHP Version: 8.0.14
 
@@ -32,16 +32,20 @@ CREATE TABLE `uploading` (
   `id` int NOT NULL,
   `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `status` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'pending',
-  `path` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `path` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `uploading`
 --
 
-INSERT INTO `uploading` (`id`, `name`, `status`, `path`) VALUES
-(1, 'img doctor ahmed', 'pending', 'uploads/16434300921069520597.jpg'),
-(2, 'img doctor Mahmoud', 'pending', 'uploads/16434301171224764610.jpg');
+INSERT INTO `uploading` (`id`, `name`, `status`, `path`, `deleted_at`, `created_at`) VALUES
+(1, 'img doctor ahmed', 'pending', 'uploads/16434300921069520597.jpg', NULL, '2022-01-29 04:44:18'),
+(4, 'gggg', 'rejected', 'uploads/16434336691165364322.jpg', '2022-01-29 06:01:16', '2022-01-29 05:21:09'),
+(5, 'ghhhhhhhhhh', 'approved', 'uploads/1643433678850636706.jpg', '2022-01-29 06:01:10', '2022-01-29 05:21:18'),
+(6, 'hsfdgfsg', 'approved', 'uploads/16434336931550554337.png', '2022-01-29 06:01:05', '2022-01-29 05:21:33');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +65,7 @@ ALTER TABLE `uploading`
 -- AUTO_INCREMENT for table `uploading`
 --
 ALTER TABLE `uploading`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
